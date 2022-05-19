12638127b0eacaabb60b10f66dbf3b7c1b63e1fe
ChrisAbdo <chris.j.abdo@gmail.com>
2022-05-19 19:43:46 -0400

    ADD: more pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div className="flex w-full justify-between items-center px-4 py-2 bg-black">
+    <div className="flex w-full justify-between items-center px-4 py-2 bg-black border-b">
         <div className="flex items-center">
             <Link href="/">
                 <h1 className="text-4xl text-white cursor-pointer font-bold">SolidityRocket🚀</h1>
---
+++ pages/simple-storage.js
@@ -0,0 +1,13 @@
+import Navbar
+ from "../components/Navbar"
+function simpleStorage() {
+  return (
+    <div>
+        <Navbar />
+        <br />
+        <h1>hi</h1>
+    </div>
+  )
+}
+
+export default simpleStorage
