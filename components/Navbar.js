3709e666473c75e453b6e74f046921b62eea9726
ChrisAbdo <chris.j.abdo@gmail.com>
2022-06-28 05:05:14 -0400

    init commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,15 +3,17 @@ import Link from 'next/link'
 
 function Navbar() {
   return (
-    <div className="flex w-full justify-between items-center px-4 py-2 bg-black border-b">
-        <div className="flex items-center">
-            <Link href="/">
-                <h1 className="text-4xl text-white cursor-pointer font-bold">SolidityRocket🚀</h1>
-            </Link>
-        </div>
-        <div className="flex items-center">
-            <ConnectButton />
-        </div>
+    <div className="flex w-full items-center justify-between border-b bg-black px-4 py-2">
+      <div className="flex items-center">
+        <Link href="/">
+          <h1 className="cursor-pointer text-4xl font-bold text-white">
+            ABD0xchange🚀
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center">
+        <ConnectButton />
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import  { NextPage } from 'next'
+import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -9,74 +9,20 @@ const Home = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-black">
       <Head>
-        <title>SolidityRocket🚀</title>
+        <title>ABD0xchange🚀</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      <img 
-       // classname hover move up to top of page.
-       className="hover:animate-bounce"
-       src="https://cdn4.iconfinder.com/data/icons/whsr-january-flaticon-set/512/rocket.png"></img>
-        <h1 className="text-6xl font-bold text-white">
-          Welcome to SolidityRocket🚀
-        </h1>
-
-        <p className="mt-3 text-2xl text-white">
-          Get started by learning about the platform.
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="/about"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:bg-gray-800"
-          >
-            <h3 className="text-2xl font-bold text-white">About &rarr;</h3>
-            <p className="mt-4 text-xl text-white">
-              Learn about SolidityRocket and the platforms goals.
-            </p>
-          </a>
-
-          <a
-            href="/example-smart-contracts"
-            className="mt-6 w-96 rounded-xl border p-6 text-left text-white hover:bg-gray-800" 
-          >
-            <h3 className="text-2xl font-bold text-white">Example Smart Contracts &rarr;</h3>
-            <p className="mt-4 text-xl">
-              View our collection of Solidity Smart Contracts to see if you like them.
-            </p>
-          </a>
-
-          <a
-            href="/generate-contract"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:bg-gray-800"
-          >
-            <h3 className="text-2xl font-bold text-white">Generate Contract &rarr;</h3>
-            <p className="mt-4 text-xl text-white">
-              Create your own Smart Contract catered to your project.
-            </p>
-          </a>
-
-          <a
-            href="/questions"
-            className="mt-6 w-96 rounded-xl border p-6 text-left text-white hover:bg-gray-800"
-          >
-            <h3 className="text-2xl font-bold text-white">Questions? &rarr;</h3>
-            <p className="mt-4 text-xl">
-              View this tab to solve any problems or find more information.
-            </p>
-          </a>
-        </div>
-      </main>
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center"></main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2 text-white"
           href="/"
         >
-          SolidityRocket🚀
+          ABD0xchange🚀
         </a>
       </footer>
     </div>
